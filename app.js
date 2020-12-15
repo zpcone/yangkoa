b4eebc382cd1a3194654038c74a271e841cfc2a2
zhaopengchao <zhaopengchao@17shanyuan.com>
2020-12-15 17:28:59 +0800

    添加文件

---
+++ app.js
@@ -0,0 +1,58 @@
+
+const Koa=require('koa'),
+
+     router=require('koa-router')(),
+    render = require('koa-art-template'),
+    path=require('path');
+    bodyParser=require('koa-bodyparser'),
+    cors = require('koa-cors'); 
+    DB=require('./module/db.js');
+
+//引入子模块
+// const cors = require('koa-cors');  
+var admin=require('./routes/admin.js');
+var api=require('./routes/api.js');
+var index=require('./routes/index.js');
+
+var app=new Koa();
+app.use(bodyParser());
+
+//配置koa-art-template 模板引擎
+render(app, {
+    root: path.join(__dirname, 'views'),
+    extname: '.html',
+    debug: process.env.NODE_ENV !== 'production'
+});
+app.use(cors({
+    origin:'http://localhost:9528',
+    credentials:true,
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    allowMethods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+ }));
+//配置路由
+router.use(index);
+/*
+  /admin   配置子路由  层级路由
+
+ /admin/user
+ */
+router.use('/admin',admin);
+/*
+ /api/newslist   新闻列表的api
+ */
+router.use('/api',api);   /*在模块里面暴露路由并且启动路由*/
+
+//启动路由
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
---
+++ module/config.js
@@ -0,0 +1,10 @@
+
+var app={
+
+    dbUrl: 'mongodb://101.201.151.186:27017/',
+
+    dbName: 'yang'
+
+}
+
+module.exports=app;
